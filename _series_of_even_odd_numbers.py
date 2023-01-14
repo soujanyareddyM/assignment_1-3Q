e88187f27945cae143c0e5761b1dc85a229993a8
soujanyareddyM <121964113+soujanyareddyM@users.noreply.github.com>
2023-01-14 21:13:25 +0530

    Add files via upload

---
+++ _series_of_even_odd_numbers.py
@@ -0,0 +1,15 @@
+num =(1,2,3,4,5,6,7,8,9)
+count_odd = 0
+count_even = 0
+for x in num:
+    if not x % 2:
+        count_even+=1
+else:
+    count_odd+=1
+    print("num of even numbers:",count_even)
+    print("num of odd numbers:",count_odd)
+
+
+    
+
+  
